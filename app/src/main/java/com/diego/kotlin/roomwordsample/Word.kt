71c37615081ececa1ea3c2d994e93155fbfb5569
balay <dlolhd@gmail.com>
2022-07-02 09:38:46 +0200

    Commit de Cración de la entidad.

---
+++ app/src/main/java/com/diego/kotlin/roomwordsample/Word.kt
@@ -0,0 +1,15 @@
+package com.diego.kotlin.roomwordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Cada clase @Entity representa una tabla SQLite.
+ * @PrimaryKey: Cada entidad necesita una clave primaria.
+ * @ColumnInfo(name = "word" ): especifica el nombre de la columna en la tabla.
+ * Cada propiedad que se almacena en la base de datos debe tener visibilidad pública, que es..
+ * ..el valor predeterminado de Kotlin.
+ */
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
